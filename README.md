fc618516cdcd6a7525d5cb543de869b089a51407
wukaitao2006 <wukaitao2006@163.com>
2017-06-22 17:38:34 +0800

    提交结构

---
+++ README.md
@@ -0,0 +1,17 @@
+# integralmall-jquery
+jQuery开发的项目
+----------
+* 安装运行
+    * 安装node
+    * 安装git
+    * 项目目录运行npm install
+    * 项目目录运行npm run dev
+----------
+* 以下为项目框架/打包注解
+    * request:请求模块
+    
+    * http-proxy-middleware:代理服务器
+    
+    * opn:浏览器打开地址
+    
+    * express:框架结构
---
+++ build/dev-server.js
@@ -0,0 +1,27 @@
+﻿var proxyMiddleware = require('http-proxy-middleware');//代理服务器
+var path = require('path');//路径中间件
+var express = require('express');//框架
+var opn = require('opn');//浏览器打开地址
+var app = express();//web框架
+var port = 6060;//端口
+
+app.use(express.static(path.join(__dirname,'../client')));//静态目录
+//代理服务器
+var proxyTable = {
+	'/shop/': {
+		target: 'http://10.141.139.159:18604/shop/',
+		changeOrigin: true,
+		pathRewrite: {
+			'^/shop/': ''
+		}
+	}
+};
+Object.keys(proxyTable).forEach(function(key){
+	const options = proxyTable[key];
+	typeof options=='string'&&(options={target: options});
+	app.use(proxyMiddleware(key,options));
+});
+app.listen(port,function(e){
+	console.log(`server start at http://localhost:${port}`);
+	opn(`http://localhost:${port}`);
+});
